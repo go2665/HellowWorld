e17c6e2865314bef283c62d2286e1eb7cc4a471b
go266 <go266@DESKTOP-I7SDLTA>
2025-09-02 11:52:29 +0900

    초기 커밋.

---
+++ HellowWorld/header.cpp
@@ -0,0 +1,8 @@
+#include "header.h"
+
+// 정의부 : 선언된 내용이 실제 어떻게 동작하는 가를 구현해놓은 곳
+
+void TestClass::TestFunc()
+{
+	// 동작 구현
+}
---
+++ HellowWorld/header.h
@@ -0,0 +1,10 @@
+#pragma once
+// 해더
+// 선언부 : ~가 있다고 미리 알려주는 것
+
+class TestClass
+{
+public:
+	int Test;
+	void TestFunc();
+};
